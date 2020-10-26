5e0ce7f6a03d9903d007cc8e532a9ffcb148e61a
sistatress <63374167+sistatress@users.noreply.github.com>
2020-10-26 08:50:00 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,116 +1,94 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./styles.css";
+import List from "./Components/List";
 
 export default function App() {
-
-  const [list, setList] = useState([])
-  const [input, setInput] = useState('')
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState("");
 
   const handleInput = (value) => {
-    
-    const newInput = value
-    
-    console.log(`[ handleInput ] newInput: ${newInput}`)
-    
-    setInput(newInput)
-  }
+    const newInput = value;
 
-  const handleInputList = (value,index) => {
-    
-    console.log(`[ handleInputList 1] index: ${index} value: ${value}`)
-    
-    const newInput = value
-    console.log(`[ handleInputList 2] newInput: ${newInput}`)
-    
-    const oldList = list
-    console.log(`[ handleInputList 3] oldList: ${oldList[index]}`)
-    
-    oldList[index] = value
-    console.log(`[ handleInputList 4] newList: ${oldList}`)
+    console.log(`[ handleInput ] newInput: ${newInput}`);
 
-    setList([...oldList])
-  }
-
-  // const updateList = (event,index) => {
-
-  //   const eventComing = event.key
-  //   console.log(`[ updateList ] event Coming ${eventComing}`)
-
-  //   // No Enter event
-  //   if(eventComing !== 'Enter') {
-  //     return;
-  //   }
-
-  //   // Enter event
-  //   const oldList = list
-  //   console.log(`[ updateList 1] oldList: ${oldList}`)
-
-  //   const value = inputTwo
-  //   console.log(`[ updateList 2] inputTwo: ${value}`)
-
-  //   oldList[index] = value
-  //   console.log(`[ oldList 3] newList: ${oldList}`)
-
-  //   setList(oldList)
-  // }
+    setInput(newInput);
+  };
 
   const saveInputTolist = (event) => {
-    
-    const eventComing = event.key
-    console.log(`[ saveInputTolist 1] event Coming ${eventComing}`)
-    
+    const eventComing = event.key;
+    console.log(`[ saveInputTolist 1] event Coming ${eventComing}`);
+
     // No Enter event
-    if(eventComing !== 'Enter') {
+    if (eventComing !== "Enter") {
       return;
     }
 
     // Enter event
-    const eventValue = event.target.value
-    console.log(`[ saveInputTolist 2] the value of the event ${eventValue}`)
-    
-    setList([...list,eventValue])
-    setInput('')
-  
-  }
+    const eventValue = event.target.value;
+    console.log(`[ saveInputTolist 2] the value of the event ${eventValue}`);
 
-  const showList = (item, index) => {
-    return (
-      <li key={index}>
-        <input
-          type='text'
-          value={item}
-          onChange={(e) => handleInputList(e.target.value, index)}
-          //onKeyPress={(event) => updateList(event, index)}
-          />
-          <button>x</button>
-      </li>
-    )
-  }
+    setList([...list, eventValue]);
+    setInput("");
+  };
 
-  //const customList =  list.map(showList)
+  const handleInputList = (value, index) => {
+    //console.log(`[ handleInputList 1] index: ${index} value: ${value}`)
 
-  const updatedInput = input
-  console.log(`[ Input ] updated input ${updatedInput}`)
+    //const newInput = value
+    //console.log(`[ handleInputList 2] newInput: ${newInput}`)
 
-  const updatedList = list
-  console.log(`[ List ] updated list ${updatedList}`)
+    const oldList = list;
+    //console.log(`[ handleInputList 3] oldList: ${oldList[index]}`)
+
+    oldList[index] = value;
+    //console.log(`[ handleInputList 4] newList: ${oldList}`)
+
+    setList([...oldList]);
+  };
+
+  const updatedInput = input;
+  console.log(`[ Input ] updated input ${updatedInput}`);
+
+  const updatedList = list;
+  console.log(`[ List ] updated list ${updatedList}`);
 
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      {/* UserInput */}
+      <h1>List-App</h1>
+      {/* UserInput : create list items  */}
       <input
-        type='text'
+        type="text"
         value={input}
         onChange={(e) => handleInput(e.target.value)}
         onKeyPress={(event) => saveInputTolist(event)}
-        />
-      <br/><br/>
-      {/* Test show a list of input */}
-      {
-        list.map(showList)
-      }
+      />
+      <br />
+      <br />
+      {/* Show a list */}
+      <List list={list} handleInputList={handleInputList} />
     </div>
   );
 }
+
+// const updateList = (event,index) => {
+
+//   const eventComing = event.key
+//   console.log(`[ updateList ] event Coming ${eventComing}`)
+
+//   // No Enter event
+//   if(eventComing !== 'Enter') {
+//     return;
+//   }
+
+//   // Enter event
+//   const oldList = list
+//   console.log(`[ updateList 1] oldList: ${oldList}`)
+
+//   const value = inputTwo
+//   console.log(`[ updateList 2] inputTwo: ${value}`)
+
+//   oldList[index] = value
+//   console.log(`[ oldList 3] newList: ${oldList}`)
+
+//   setList(oldList)
+// }
---
+++ src/Components/List.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const List = ({ list, handleInputList }) => {
+  const showList = (item, index) => {
+    return (
+      <li key={index}>
+        <input
+          type="text"
+          value={item}
+          onChange={(e) => handleInputList(e.target.value, index)}
+          //onKeyPress={(event) => updateList(event, index)}
+        />
+        <button>x</button>
+      </li>
+    );
+  };
+
+  return (
+    <div>
+      <h1>List Component</h1>
+      {list.map(showList)}
+    </div>
+  );
+};
+
+export default List;
